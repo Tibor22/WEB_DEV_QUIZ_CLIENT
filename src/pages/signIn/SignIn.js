0198f4c61dfd9b435c8160951f9a74a8a1a44b3e
Tibor22 <tothtibitravel@gmail.com>
2022-09-22 13:32:00 +0100

    created UI for login

--- src/pages/signIn/SignIn.js
+++ src/pages/signIn/SignIn.js
@@ -1,5 +1,32 @@
 import './signIn.css';
-
+import { FcGoogle } from 'react-icons/fc';
 export default function SignIn() {
-	return <div className='signIn-outer-container'></div>;
+	const styles = {
+		marginRight: '2rem',
+	};
+	return (
+		<div className='signIn-outer-container'>
+			<div className='signIn-container'>
+				<div className='signIn-component'>
+					<h2 className='signIn-component__header'>SIGN IN</h2>
+					<p className='signIn-component__legal'>
+						By using Google to Log in, your personal details are safe. We do not
+						gain access to your password or private information.
+					</p>
+					<div className='signIn-component__login-btns'>
+						<button className='google-btn sign-in-btn'>
+							<div className='google-icon'>
+								{' '}
+								<FcGoogle size='2.3rem' styles={styles} />
+							</div>
+
+							<span className='google-text'>Sign in with Google</span>
+						</button>
+						<div className='divider'>OR</div>
+						<button className='sign-in-btn'>Sign in as guest</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
