5b26938c36ee43cde2ba4e1ea2a9413db1a2e31c
Dan <dannorthcott8@gmail.com>
2022-09-29 18:37:55 +0100

    added categories component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,5 +1,45 @@
 import './categories.css';
+import {
+    SiNodedotjs,
+    SiReact,
+    SiJavascript,
+    SiTypescript,
+} from 'react-icons/si';
+import { TbArrowsShuffle } from 'react-icons/tb';
+import { GiFamilyTree } from 'react-icons/gi';
+import { IconContext } from 'react-icons';
 
-export default function Categories() {
-	return <div className='categories-container'></div>;
+export default function Categories({ setCategory, category }) {
+    const handleClick = (e) => {};
+
+    return (
+        <div className='categories-container'>
+            <IconContext.Provider value={{ size: '7rem', color: 'white' }}>
+                <div onClick={handleClick}>
+                    <SiNodedotjs />
+                    <span>Node.Js</span>
+                </div>
+                <div onClick={handleClick} id='react'>
+                    <SiReact />
+                    <span>React</span>
+                </div>
+                <div onClick={handleClick} id='javascript'>
+                    <SiJavascript />
+                    <span>Javascript</span>
+                </div>
+                <div onClick={handleClick} id='typescript'>
+                    <SiTypescript />
+                    <span>TypeScript</span>
+                </div>
+                <div onClick={handleClick} id='algorithms'>
+                    <TbArrowsShuffle />
+                    <span>Algorithms</span>
+                </div>
+                <div onClick={handleClick} id='data-structures'>
+                    <GiFamilyTree />
+                    <span>Data Structures</span>
+                </div>
+            </IconContext.Provider>
+        </div>
+    );
 }
--- src/components/singlePlayer/SinglePlayer.js
+++ src/components/singlePlayer/SinglePlayer.js
@@ -2,22 +2,24 @@ import './singlePlayer.css';
 import Categories from '../Categories/Categories.js';
 
 export default function SinglePlayer() {
-	return (
-		<div className='singlePlayer-container'>
-			<h1 className='singlePlayer-container__header'>SINGLE PLAY</h1>
-			<div className='singlePlayer-container__categories'>
-				<h2 className='singlePlayer-container__categories--header'></h2>
-				<div className='singlePlayer-container__categories--container'>
-					<Categories />
-				</div>
-			</div>
-			<div className='singlePlayer-container__start-btn-container'>
-				<button className='singlePlayer-container__start-btn offset'>
-					START GAME
-				</button>
-			</div>
+    return (
+        <div className='singlePlayer-container'>
+            <h1 className='singlePlayer-container__header'>SINGLE PLAY</h1>
+            <div className='singlePlayer-container__categories'>
+                <h2 className='singlePlayer-container__categories--header'>
+                    Please choose a category:
+                </h2>
+                <div className='singlePlayer-container__categories--container'>
+                    <Categories />
+                </div>
+            </div>
+            <div className='singlePlayer-container__start-btn-container'>
+                <button className='singlePlayer-container__start-btn offset'>
+                    START GAME
+                </button>
+            </div>
 
-			<div className='singlePlayer-container__leaderboard'></div>
-		</div>
-	);
+            <div className='singlePlayer-container__leaderboard'></div>
+        </div>
+    );
 }
